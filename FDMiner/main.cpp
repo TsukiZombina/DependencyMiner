6552b84f6ec477b37b2f108af4f96ef8e9f9a752
Calcifer <calciferzh@outlook.com>
2018-05-16 10:46:37 +0800

    naive reader

--- FDMiner/main.cpp
+++ FDMiner/main.cpp
@@ -1,3 +1,10 @@
+#include "TANE.hpp"
+#include <string>
+
 int main() {
+    // std::string path = "./data/test_data.txt";
+    std::string path = "./data/data.txt";
+    TANE t = TANE(path);
+    system("pause");
     return 0;
 }
