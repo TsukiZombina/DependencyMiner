5c9ffe6c5bbbc3b4081055740ce7cf48c5ade5de
TsukiZombina <ammy_zmmn@hotmail.com>
2019-05-06 02:08:57 -0500

    Add similarity_metrics.hpp to reader

--- FDMiner/reader.hpp
+++ FDMiner/reader.hpp
@@ -6,8 +6,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
+#include "similarity_metrics.hpp"
 
 class Reader {
 public:
@@ -17,8 +20,8 @@ public:
   // vector containing all the column names
   std::vector<std::string> attributes;
 
-  int nrow;
-  int ncol;
+  unsigned int nrow;
+  unsigned int ncol;
 
   Reader(std::string path) {
     // std::cout << "Reading data: " << path << std::endl;
@@ -54,9 +57,9 @@ public:
   }
 
   void parse_line(std::string& line, std::vector<int>& row) {
-    int left = 0;
-    int right = 0;
-    int col_idx = 0;
+    unsigned int left = 0;
+    unsigned int right = 0;
+    unsigned int col_idx = 0;
     while (right < line.length()) {
       if (line[right] == ',' && line[right + 1] != ' ') {
         // a new column
@@ -74,24 +77,45 @@ public:
     row.push_back(code);
   }
 
-  int hash(std::string& value, int col_idx) {
-    if (col_idx >= value_map.size()) {
-      value_map.emplace_back(std::unordered_map<std::string, int>());
-      value_map[value_map.size() - 1].reserve(100000);
-    }
-    auto iter = value_map[col_idx].find(value);
-    if (iter != value_map[col_idx].end()) {
-      return iter->second;
-    } else {
+  int hash(std::string& value, unsigned int col_idx)
+  {
+      if(col_idx >= value_map.size())
+      {
+          value_map.emplace_back(std::unordered_map<std::string, int>());
+          value_map[value_map.size() - 1].reserve(100000);
+      }
+
+      for(const auto& v: value_map[col_idx])
+      {
+          if(isNumeric(v.first) && isNumeric(value))
+          {
+              //std::cout << v.first << ", " << value << std::endl;
+              float d = std::fabs(std::stof(v.first) - std::stof(value));
+
+              if(d < 3)
+              {
+                  return v.second;
+              }
+          }
+          else
+          {
+              size_t d = uiLevenshteinDistance(v.first, value);
+
+              if(d < 1)
+              {
+                  return v.second;
+              }
+          }
+      }
+
       int code = value_map[col_idx].size();
       value_map[col_idx][value] = code;
-      return code;
-    }
-  }
 
+      return code;
+  }
   bool check_integrity() {
     bool flag = true;
-    for (int i = 0; i < nrow; ++i) {
+    for (unsigned int i = 0; i < nrow; ++i) {
       if (data[i].size() != ncol) {
         std::cout << "Error!\n"
                   << "But row " << i << " has " << data[i].size() << " columns.\n";
@@ -101,6 +125,10 @@ public:
     std::cout << "Total Rows: " << nrow << "\n"
               << "Total Cols: " << ncol << "\n";
     return flag;
+  }
+
+  bool isNumeric(const std::string& input) {
+      return std::all_of(input.begin(), input.end(), ::isdigit);
   }
 };
 
