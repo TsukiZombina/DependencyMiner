4a4c011ea5bf462eca8fecdcde2b28295385bdc5
TsukiZombina <ammy_zmmn@hotmail.com>
2019-06-02 23:26:14 -0500

    Change prefix and complete format list.

--- FDMiner/tools/run_similarity.py
+++ FDMiner/tools/run_similarity.py
@@ -1,7 +1,7 @@
 import subprocess
 import numpy as np
 
-prefix = "pato_"
+prefix = "Sim_"
 
 L = np.array([1, 2, 3])
 
@@ -11,7 +11,7 @@ D = np.array([1, 2, 7, 15, 30])
 
 w_start = 0.0001
 w_end   = 0.01
-W = np.linspace(w_start, w_end, 5);
+W = np.linspace(w_start, w_end, 5)
 
 print(W)
 
@@ -20,10 +20,10 @@ for l in L:
         for t in T:
             for d in D:
                 filename = prefix + "w=" + "{0:.3f}".format(w) + "_l=" + str(l)
-                subprocess.run(["../debug/DepMinerTsuki.exe",
+                subprocess.run(["../build/DepMinerTsuki.exe",
                     "-f", "../test.csv",
                     "-o", filename,
                     "-s", "1",
                     "-a", "0,1,2,3,4,5,6,7,8,9,10,11,12,13",
                     "-m", "t,t,d,l,l,l,l,l,l,l,l,l,l,w",
-                    "-v", "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}".format(l,w)])
+                    "-v", "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}".format(t,t,d,l,l,l,l,l,l,l,l,l,l,w)])
