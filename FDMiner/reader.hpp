931709bddfa532f043c6c39061cf0fc04882ddf3
TsukiZombina <ammy_zmmn@hotmail.com>
2019-10-22 16:44:07 -0500

    Change algorithm to find nearest center

--- FDMiner/reader.hpp
+++ FDMiner/reader.hpp
@@ -11,6 +11,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <regex>
+#include <cfloat>
 #include "similarity_metrics.hpp"
 
 class Reader {
@@ -20,6 +21,8 @@ public:
   std::vector<std::unordered_map<std::string, int>> value_map;
   // vector containing all the column names
   std::vector<std::string> attributes;
+  // unordered_map holding centers for clustering
+  std::unordered_map<int, std::vector<std::string>> centersMap;
 
   unsigned int nrow;
   unsigned int ncol;
@@ -68,6 +71,27 @@ public:
     }
     nrow = data.size();
     ncol = value_map.size();
+
+    std::ofstream ecStream("eq_classes.txt");
+
+    for(const auto& attribute: attributes)
+    {
+        ecStream << attribute << " ";
+    }
+
+    ecStream << std::endl;
+
+    for(const auto& row: data)
+    {
+        for(const auto& ec: row)
+        {
+            ecStream << ec << "\t";
+        }
+
+        ecStream << std::endl;
+    }
+
+    ecStream.close();
   }
 
   void parse_line(std::string& line, std::vector<int>& row) {
@@ -128,131 +152,163 @@ public:
     }
   }
 
-  int sim_hash(std::string& value, unsigned int col_idx)
+  int sim_hash(std::string& value, int col_idx)
   {
-      if(col_idx >= value_map.size())
-      {
+      if (col_idx >= value_map.size()) {
           value_map.emplace_back(std::unordered_map<std::string, int>());
           value_map[value_map.size() - 1].reserve(100000);
       }
 
-      for(const auto& v: value_map[col_idx])
+      if(_indices.count(col_idx) > 0)
       {
-          if(_indices.count(col_idx) > 0)
+          if(centersMap.count(col_idx) == 0)
           {
-              std::string metric = _indices[col_idx].first;
-
-              if(metric == "d")
-              {
-                  // Parse date strings
-                  std::regex re("[/]+");
-                  std::sregex_token_iterator it1(v.first.begin(), v.first.end(), re, -1);
-                  std::sregex_token_iterator it2(value.begin(), value.end(), re, -1);
-                  std::sregex_token_iterator reg_end;
-
-                  int d1[3] = { 0, 0, 0 };
-                  int d2[3] = { 0, 0, 0 };
-
-                  for(int i = 0; it1 != reg_end && it2 != reg_end; ++it1, ++it2, ++i)
-                  {
-                      d1[i] = std::stoi(it1->str());
-                      d2[i] = std::stoi(it2->str());
-                  }
-
-                  Date date1 = { d1[0], d1[1], d1[2] };
-                  Date date2 = { d2[0], d2[1], d2[2] };
-
-                  unsigned int d = getDifference(date1, date2);
-                  long threshold = std::stol(_indices[col_idx].second);
-
-                  if(d < threshold)
-                  {
-                      return v.second;
-                  }
-              }
-              else if(metric == "t")
-              {
-                  // Parse time strings
-                  std::regex re("[:]");
-                  std::sregex_token_iterator it1(v.first.begin(), v.first.end(), re, -1);
-                  std::sregex_token_iterator it2(value.begin(), value.end(), re, -1);
-                  std::sregex_token_iterator reg_end;
-
-                  unsigned t1[2] = { 0, 0 };
-                  unsigned t2[2] = { 0, 0 };
-
-                  for(int i = 0; it1 != reg_end && it2 != reg_end; ++it1, ++it2, ++i)
-                  {
-                      t1[i] = std::stoi(it1->str());
-                      t2[i] = std::stoi(it2->str());
-                  }
-
-                  Time time1 = { t1[0], t1[1] };
-                  Time time2 = { t2[0], t2[1] };
-
-                  long d = getDifference(time1, time2);
-                  long threshold = std::stol(_indices[col_idx].second);
-
-                  if(d < threshold)
-                  {
-                      return v.second;
-                  }
-              }
-              else if(metric == "w")
-              {
-                  // Parse vector strings
-                  std::regex re("[ ]+");
-                  std::sregex_token_iterator it1(v.first.begin(), v.first.end(), re, -1);
-                  std::sregex_token_iterator it2(value.begin(), value.end(), re, -1);
-                  std::sregex_token_iterator reg_end;
-
-                  std::vector<float> x;
-                  std::vector<float> y;
-
-                  for(; it1 != reg_end && it2 != reg_end; ++it1, ++it2)
-                  {
-                      x.push_back(std::stof(it1->str()));
-                      y.push_back(std::stof(it2->str()));
-                  }
-
-                  float d = getDifference(x, y);
-                  float threshold = std::stof(_indices[col_idx].second);
-
-                  if(d < threshold)
-                  {
-                      return v.second;
-                  }
-              }
-              else if(metric == "l")
-              {
-                  size_t d = getDifference(v.first, value);
-                  float threshold = std::stoi(_indices[col_idx].second);
-
-                  if(d < threshold)
-                  {
-                      return v.second;
-                  }
-              }
+              centersMap[col_idx] = std::vector<std::string>();
+              centersMap[col_idx].push_back(value);
+              return 0;
           }
           else
           {
-              auto iter = value_map[col_idx].find(value);
-              if (iter != value_map[col_idx].end()) {
-                  return iter->second;
-              }
-              else
+              std::string metric = _indices[col_idx].first;
+
+              int eqClass = -1;
+              float minDistance = FLT_MAX;
+
+              for(int j = 0; j < centersMap[col_idx].size(); j++)
               {
-                  int code = value_map[col_idx].size();
-                  value_map[col_idx][value] = code;
+                  if(metric == "w")
+                  {
+                      // Parse strings into numeric vector
+                      std::regex re("[ ]+");
+                      std::sregex_token_iterator it1(centersMap[col_idx][j].begin(), centersMap[col_idx][j].end(), re, -1);
+                      std::sregex_token_iterator it2(value.begin(), value.end(), re, -1);
+                      std::sregex_token_iterator reg_end;
+
+                      std::vector<float> x;
+                      std::vector<float> y;
+
+                      for(; it1 != reg_end && it2 != reg_end; ++it1, ++it2)
+                      {
+                          x.push_back(std::stof(it1->str()));
+                          y.push_back(std::stof(it2->str()));
+                      }
+
+                      float d = getDifference(x, y);
+                      float delta = std::stof(_indices[col_idx].second);
+
+                      if(d <= delta)
+                      {
+                          if(d < minDistance)
+                          {
+                              minDistance = d;
+                              eqClass = j;
+                          }
+                      }
+                  }
+                  else if(metric == "d")
+                  {
+                      // Parse date strings
+                      std::regex re("[/]+");
+                      std::sregex_token_iterator it1(centersMap[col_idx][j].begin(), centersMap[col_idx][j].end(), re, -1);
+                      std::sregex_token_iterator it2(value.begin(), value.end(), re, -1);
+                      std::sregex_token_iterator reg_end;
+
+                      int d1[3] = { 0, 0, 0 };
+                      int d2[3] = { 0, 0, 0 };
+
+                      for(int i = 0; it1 != reg_end && it2 != reg_end; ++it1, ++it2, ++i)
+                      {
+                          d1[i] = std::stoi(it1->str());
+                          d2[i] = std::stoi(it2->str());
+                      }
+
+                      Date date1 = { d1[0], d1[1], d1[2] };
+                      Date date2 = { d2[0], d2[1], d2[2] };
+
+                      int d = getDifference(date1, date2);
+                      long delta = std::stol(_indices[col_idx].second);
+
+                      if(d <= delta)
+                      {
+                          if(d < minDistance)
+                          {
+                              minDistance = d;
+                              eqClass = j;
+                          }
+                      }
+                  }
+                  else if(metric == "t")
+                  {
+                      // Parse time strings
+                      std::regex re("[:]");
+                      std::sregex_token_iterator it1(centersMap[col_idx][j].begin(), centersMap[col_idx][j].end(), re, -1);
+                      std::sregex_token_iterator it2(value.begin(), value.end(), re, -1);
+                      std::sregex_token_iterator reg_end;
+
+                      unsigned t1[2] = { 0, 0 };
+                      unsigned t2[2] = { 0, 0 };
+
+                      for(int i = 0; it1 != reg_end && it2 != reg_end; ++it1, ++it2, ++i)
+                      {
+                          t1[i] = std::stoi(it1->str());
+                          t2[i] = std::stoi(it2->str());
+                      }
+
+                      Time time1 = { t1[0], t1[1] };
+                      Time time2 = { t2[0], t2[1] };
+
+                      long d = getDifference(time1, time2);
+                      long threshold = std::stol(_indices[col_idx].second);
+
+                      if(d <= threshold)
+                      {
+                          if(d < minDistance)
+                          {
+                              minDistance = d;
+                              eqClass = j;
+                          }
+                      }
+                  }
+                  else if(metric == "l")
+                  {
+                      size_t d = getDifference(centersMap[col_idx][j], value);
+                      float threshold = std::stoi(_indices[col_idx].second);
+
+                      if(d <= threshold)
+                      {
+                          if(d < minDistance)
+                          {
+                              minDistance = d;
+                              eqClass = j;
+                          }
+                      }
+                  }
+              }
+
+              if(eqClass == -1)
+              {
+                  // Add new center
+                  int code = centersMap[col_idx].size();
+                  centersMap[col_idx].push_back(value);
                   return code;
               }
+
+              return eqClass;
           }
       }
 
-      int code = value_map[col_idx].size();
-      value_map[col_idx][value] = code;
+      auto iter = value_map[col_idx].find(value);
 
-      return code;
+      if (iter != value_map[col_idx].end())
+      {
+          return iter->second;
+      }
+      else
+      {
+          int code = value_map[col_idx].size();
+          value_map[col_idx][value] = code;
+          return code;
+      }
   }
 
   bool check_integrity() {
